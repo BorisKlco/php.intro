beb9194b629662d6f284e441f08b338783b77864
Boris Klco <klco@pm.me>
2024-07-10 12:48:17 +0200

    composer dump-autoload -o

--- oop/autoloading/index.php
+++ oop/autoloading/index.php
@@ -2,16 +2,18 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/vendor/autoload.php';
+
 //require_once './PaymentGateway/Stripe/Transaction.php';
 
-spl_autoload_register(function($class) {
-    $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+// spl_autoload_register(function ($class) {
+//     $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
 
-    if (file_exists($path)) {
-        require $path;
-    }
-    var_dump($path);
-});
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+//     var_dump($path);
+// });
 
 
 use PaymentGateway\Stripe\Transaction;
