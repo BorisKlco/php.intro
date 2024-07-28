c95048dc0e050d26e58b0ca5587d36ded5e7faae
BorisKlco <klco@pm.me>
2024-07-28 15:41:01 +0000

    insert to db

--- csv-site/app/src/Controller/Upload.php
+++ csv-site/app/src/Controller/Upload.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\DB;
+
 class Upload
 {
-    public function store(array $files): string
+    public function store(array $files): self
     {
 
         $arr = [];
@@ -24,6 +26,50 @@ class Upload
             move_uploaded_file($fileTmp, $filePath);
         }
 
-        return 'ok';
+        return $this;
+    }
+
+    public function process()
+    {
+        $db = DB::connection();
+        $files = scandir(STORAGE);
+        foreach ($files as $current) {
+            $path = STORAGE . $current;
+            if (is_file($path)) {
+                $this->extract($path, $db);
+            }
+        }
+    }
+
+    private function extract(string $path, \PDO $db)
+    {
+        $open_file = fopen($path, 'r');
+        $header = fgetcsv($open_file);
+
+        $insert_transaction = $db->prepare(
+            'INSERT INTO transactions (transaction_date,check_id,info,amount,added)
+            VALUES(:transaction_date, :check_id, :info, :amount, NOW())'
+        );
+
+        while (($line = fgetcsv($open_file)) !== false) {
+            try {
+                $db->beginTransaction();
+                $format_date = \DateTime::createFromFormat('d/m/Y', $line[0])->format('Y-m-d');
+                $insert_transaction->execute(
+                    [
+                        'transaction_date' => $format_date,
+                        'check_id' => empty($line[1]) ? null : $line[1],
+                        'info' => $line[2],
+                        'amount' => (float) str_replace(['$', ','], '', $line[3]),
+                    ]
+                );
+                $db->commit();
+            } catch (\Throwable) {
+                if ($db->inTransaction()) {
+                    $db->rollBack();
+                }
+            }
+        }
+        unlink($path);
     }
 }
